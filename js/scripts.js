62391a21a397aeacb9b8b98271b4bc0c7ad5dc58
Suzanne Schuber <suzanneschuber@gmail.com>
2023-06-03 13:20:01 -0700

    update css and html with img.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ function hideLang() {
   document.getElementById("ruby").setAttribute("class", "hidden");
   document.getElementById("c#").setAttribute("class", "hidden");
   document.getElementById("python").setAttribute("class", "hidden")
+  document.getElementById("whatTheHeck").setAttribute("class", "hidden")
+
 }
 
 window.onload = function () {
@@ -14,19 +16,20 @@ window.onload = function () {
     let gummyBears = document.querySelector("input#gummyBears").value;
     let metal = document.querySelector("input#metal").value;
     let hats = document.querySelector("input#hats").value;
-    let pizza = document.querySelector('input[name="pizza"]:checked').value;
+    let pizza = document.querySelector('input#pizza').value
 
-    if (pizza === "no") {
-      document.getElementById("whatTheHeck").removeAttribute("class");
-
+    if (gummyBears > 5 && cats > 1) {
+      document.getElementById("python").removeAttribute("class");
     }
+    if (hats < 3 && metal === "gold", "Gold") {
+      document.getElementById("c#").removeAttribute("class")
+    }
+    if (gummyBears < 5 && cats === 0) {
+      document.getElementById("ruby").removeAttribute("class")
+    }
+    if (pizza === "No", "no", "NO") {
+      document.getElementById("whatTheHeck").removeAttribute("class")
+    }
+  }
 
-
-
-
-
-
-
-
-  };
 };
